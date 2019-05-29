a429a9c8a7b580f8c7c561bd0b75e64aa8e6d2a9
Tian Ming <tianming@Tians-iMac.local>
2019-05-29 17:22:53 +0800

    init app

---
+++ MicroChat-ios/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MicroChat-ios
+//
+//  Created by Tian Ming on 2019/5/29.
+//  Copyright © 2019 handsomeMT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'MicroChat-ios' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MicroChat-ios
+  pod 'SnapKit', '~> 4.2.0'
+  pod 'ReactiveCocoa', '~> 9.0.0'
+
+end
